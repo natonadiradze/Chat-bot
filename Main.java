a459fab75dc40f5f8b22315ae89e7da7201a72cf
natonadiradze <84264348+natonadiradze@users.noreply.github.com>
2022-09-25 14:38:31 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.awt.desktop.SystemEventListener;
+//nato nadiradze
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello there, my name is chatbot. How can i help you?");
+        System.out.println(">>>");
+        String n = sc.nextLine();
+        System.out.println("Okay " + n + " i will try to solve your problem");
+
+    }
+}
